1f1198f28869c8da41863ad754eda1309b403b1f
Christian Kaus <christian.kaus@hotmail.de>
2015-11-14 00:39:10 +0100

    change design settings and add description + link to github

--- src/ui/aboutdialog.py
+++ src/ui/aboutdialog.py
@@ -6,9 +6,13 @@ from PyQt4 import uic, QtCore
 path = os.path.dirname(os.path.abspath(__file__))
 AboutDialogUI, AboutDialogBase = uic.loadUiType(
     os.path.join(path, "aboutdialog.ui"))
+topText = "EpyPy 1.0"
+bottomText = "A Tool for fitting epidemic models.<br />Source: <a href=\"https://github.com/ckaus/EpiPy\">GitHub</a>"
 
 class AboutDialog(AboutDialogBase, AboutDialogUI):
     def __init__(self, parent=None):
     	AboutDialogBase.__init__(self, parent)
     	self.setupUi(self)
-    	self.okButton.clicked.connect(self.close)
+    	self.topLabel.setText(topText);
+    	self.bottomLabel.setText(bottomText);
+    	self.closeButton.clicked.connect(self.close)
