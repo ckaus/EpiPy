6631812b953baae825bb6f47323ef5e376a0badb
Christian Kaus <christian.kaus@hotmail.de>
2015-11-14 02:44:09 +0100

    add save/clear button for fitting information

--- src/ui/mainwindow.py
+++ src/ui/mainwindow.py
@@ -1,58 +1,81 @@
 # -*- coding: utf-8 -*-
 
 import os
-from PyQt4 import uic, QtCore
+from PyQt4 import uic, QtCore, QtGui
 from aboutdialog import AboutDialog
+from utils import logger
 
-path = os.path.dirname(os.path.abspath(__file__))
+filePath = os.path.abspath(__file__)
+folderPath = os.path.dirname(filePath)
 MainWindowUI, MainWindowBase = uic.loadUiType(
-    os.path.join(path, "mainwindow.ui"))
+    os.path.join(folderPath, "mainwindow.ui"))
 
 class MainWindow(MainWindowBase, MainWindowUI):
     def __init__(self):
     	MainWindowBase.__init__(self)
     	self.setupUi(self)
     	self.showSidebar()
-    	# disable first item
-    	self.modelComboBox.setItemData(0, QtCore.QVariant(0),QtCore.Qt.UserRole -1)
-    	# connect menu components with functions
-    	self.openFileAction.triggered.connect(self.openFile)
-    	self.saveAction.triggered.connect(self.save)
-    	self.saveAsAction.triggered.connect(self.saveAs)
-    	self.exportAction.triggered.connect(self.export)
-    	self.exitAction.triggered.connect(self.close)
+        # menu
+        # connect menu components with functions
+        self.openFileAction.triggered.connect(self.openFile)
+        self.saveAction.triggered.connect(self.save)
+        self.saveAsAction.triggered.connect(self.saveAs)
+        self.exportAction.triggered.connect(self.export)
+        self.exitAction.triggered.connect(self.close)
         self.showFullscreenAction.triggered.connect(self.showFullscreen)
         self.exitFullscreenAction.triggered.connect(self.exitFullscreen)
         self.exitFullscreenAction.setVisible(False)
         self.showSidebarAction.triggered.connect(self.showSidebar)
-    	self.hideSidebarAction.triggered.connect(self.hideSidebar)
-    	self.showSidebarAction.setVisible(False)
+        self.hideSidebarAction.triggered.connect(self.hideSidebar)
+        self.showSidebarAction.setVisible(False)
         self.aboutAction.triggered.connect(self.showAbout)
+        # side bar
+        # disable first item of model box in option groupbox
+    	self.modelComboBox.setItemData(0, QtCore.QVariant(0),QtCore.Qt.UserRole -1)
+    	# fitting information groupbox
+        self.clearButton.setIcon(QtGui.QIcon("../resources/clear.png"))
+        self.clearButton.clicked.connect(self.infoTextEdit.clear)
+        self.saveButton.setIcon(QtGui.QIcon("../resources/save.png"))
+        self.searchLineEdit.returnPressed.connect(self.searchInfoText)
 
     def openFile(self):
-    	print "open file"
+    	logger.info("open file")
+        self.infoTextEdit.appendPlainText(logger.info("open file"))
+    
     def save(self):
-    	print "save"
+    	logger.info("save")
+        self.infoTextEdit.appendPlainText(logger.info("save"))
+    
     def saveAs(self):
-    	print "save as"
+    	logger.info("save as")
+        self.infoTextEdit.appendPlainText(logger.info("save as"))
+    
     def export(self):
-    	print "export"
+    	logger.info("export")
+        self.infoTextEdit.appendPlainText(logger.info("export"))
+    
     def showFullscreen(self):
         self.showFullscreenAction.setVisible(False)
         self.exitFullscreenAction.setVisible(True)
         self.showFullScreen()
+    
     def exitFullscreen(self):
         self.showFullscreenAction.setVisible(True)
         self.exitFullscreenAction.setVisible(False)
         self.showNormal()
+    
     def showSidebar(self):
     	self.sidebarGroupBox.setVisible(True)
     	self.showSidebarAction.setVisible(False)
     	self.hideSidebarAction.setVisible(True)
+    
     def hideSidebar(self):
     	self.sidebarGroupBox.setVisible(False)
     	self.showSidebarAction.setVisible(True)
     	self.hideSidebarAction.setVisible(False)
+    
     def showAbout(self):
-    	aboutDialog = AboutDialog(self)
-        aboutDialog.show()
+    	AboutDialog(self).show()
+    
+    def searchInfoText(self):
+        self.infoTextEdit.appendPlainText(logger.info("search"))
