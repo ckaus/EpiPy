96073379f84e6bdde72008b5389f6107f90273cc
Christian Kaus <christian.kaus@hotmail.de>
2015-11-14 02:44:48 +0100

    define return values for logging in gui

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -18,13 +18,19 @@ def error(msg):
 	func = inspect.stack()[1][3]
 	
 	print '[%s%s%s] class:%s in %s() on line %s\n\tMessage: %s'\
-	% (_Color.ERROR,'ERROR',_Color.NORMAL, clasz,func, line, msg)
+	% (_Color.ERROR, 'ERROR', _Color.NORMAL, clasz,func, line, msg)
+	
+	return '[%s] class:%s in %s() on line %s\n\tMessage: %s'\
+	% ('ERROR', clasz, func, line, msg)
 
 def info(msg):
-	print '[%s%s%s] %s' % (_Color.INFO,'INFO',_Color.NORMAL,msg)
+	print '[%s%s%s] %s' % (_Color.INFO, 'INFO', _Color.NORMAL, msg)
+	return '[%s] %s' % ('INFO', msg)
 
 def success(msg):
-	print '[%s%s%s] %s' % (_Color.SUCCESS,'SUCCESS',_Color.NORMAL,msg)
+	print '[%s%s%s] %s' % (_Color.SUCCESS, 'SUCCESS', _Color.NORMAL, msg)
+	return '[%s%s%s] %s' % ('SUCCESS', msg)
 
 def warning(msg):
-	print '[%s%s%s] %s' % (_Color.WARNING,'WARNING',_Color.NORMAL,msg)
+	print '[%s%s%s] %s' % (_Color.WARNING, 'WARNING', _Color.NORMAL,msg)
+	return '[%s%s%s] %s' % ('WARNING', msg)
