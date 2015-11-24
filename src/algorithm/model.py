c8f8becb3f024f847443f02e05de841f3484b05d
Christian Kaus <christian.kaus@hotmail.de>
2015-11-24 14:48:43 +0100

    try to optimize sir model

--- src/algorithm/model.py
+++ src/algorithm/model.py
@@ -2,7 +2,21 @@
 
 import numpy as np
 import scipy.integrate as spi
-import pylab as pl
+import math
+
+# only infected values
+dataset = [1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 4, 5, 5,
+            6, 7, 7, 7, 8, 8, 9, 9, 9, 9, 10, 10, 
+            13, 14, 13, 10, 10, 10, 11, 10, 10, 10, 11, 
+            10, 10, 10, 8, 8, 7, 8, 10, 9, 11, 11, 12, 12, 
+            14, 14, 12, 13, 17, 19, 17, 16, 17, 18, 20, 19, 16, 
+            16, 15, 14, 15, 15, 16, 17, 17, 17, 18, 17, 17, 18, 
+            19, 21, 21, 23, 26, 26, 25, 26, 31, 31, 30, 36, 39, 
+            48, 48, 51, 57, 56, 61, 67, 68, 66, 67, 67, 66, 64, 60, 
+            57, 55, 56, 57, 53, 52, 50, 49, 49, 48, 45, 44, 43, 44, 
+            43, 40, 37, 39, 40, 41, 41, 38, 34, 35, 33, 32, 32, 29, 28, 
+            11, 12, 12, 12, 12, 12, 12, 13, 13, 12, 12, 10, 10, 11, 9, 10, 
+            9, 8, 6, 6, 6, 6, 4, 3, 3, 4, 4, 4, 1, 1, 0]
 
 beta=1.4247
 gamma=0.14286
@@ -16,7 +30,7 @@ def sir(parameters, time):
     :param time: a sequence of time points for which to solve for parameters.
     :returns: array of points for S, I, R
     """
-    sir_v=np.zeros((3))
+    sir_v = np.zeros((3))
     sir_v[0] = - beta * parameters[0] * parameters[1]
     sir_v[1] = beta * parameters[0] * parameters[1] - gamma * parameters[1]
     sir_v[2] = gamma * parameters[1]
@@ -32,3 +46,15 @@ def compute(model, time=np.arange(0.0, 71.0, 1)):
     S0=1-1e-6 # 0.999999
     I0=1e-6 # -3.28171817154
     return spi.odeint(model, (S0, I0, 0.0), time)
+
+if __name__ == '__main__':
+    sum_target = 0.3
+    # build sir model
+    sir_model = compute(sir,np.arange(0.0,len(dataset),1))
+    # iterate through dataset and calculate the difference between
+    # expected Infected value and dataset value at position i
+    for i in range(len(dataset)):
+        sum_of_sq = math.pow(dataset[i] - sir_model[i,1],2)
+        if sum_of_sq > sum_target:    
+            # optimze infected value of sir model
+            print sum_of_sq
