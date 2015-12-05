a487dedab855b0efc686c530b5950d3998824134
Christian Kaus <christian.kaus@hotmail.de>
2015-12-05 15:20:32 +0100

    optimize csvmanager, you can read special coloumns

--- src/utils/csvmanager.py
+++ src/utils/csvmanager.py
@@ -5,15 +5,17 @@ import csv
 import logger
 
 current_dir =  os.path.abspath(os.path.dirname(__file__))
-resources_dir = os.path.abspath(current_dir + "/../../resources")
+resources_dir = os.path.abspath(current_dir + "/../../resources/data")
 
-def read(file_name, seperator=";"):
+def read(file_name, seperator=";", col=[]):
 	"""
 	This function reads a csv file.
 	
 	:param file_name: a file name
 	:param type: str
-	
+	:param: col: readable coloumn(s)
+	:param type: list
+
 	:returns: the content as *Dictionary*
 	:raises: *Error* if csv file cannot read
 	"""
@@ -22,15 +24,26 @@ def read(file_name, seperator=";"):
 	reader = csv.reader(file, delimiter=seperator)
 	result = {}
 	header = reader.next()
-	# read header
+	
 	try:
-		for h in header:
-			result[h] = []
-		# read content 
-		for row in reader:
-			# match with header field
-			for h, v in zip(header, row):
-				result[h].append(float(v))
+		if len(col) > 0: # read only given coloumns
+			# header
+			for c in col:
+				if c in header:
+					result[c] = []
+			# content
+			for row in reader:
+				for c in col:
+					result[c].append(row[header.index(c)])
+		else: # read all coloumns
+			# header
+			for h in header:
+				result[h] = []
+			# content 
+			for row in reader:
+				# match content with header
+				for h, v in zip(header, row):
+					result[h].append(v)
 	except csv.Error as e:
 		logger.error("Can not read file %s, %s" % (filename,  e))
 	return result
