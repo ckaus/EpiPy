cc073d459c1f671dee00b7fcfc48663676377a40
Christian Kaus <christian.kaus@hotmail.de>
2016-01-08 20:46:32 +0100

    remove print statement

--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Comment out the installation instructions for your operating system
-
-# Unix
-# ----
-# sudo pip install -r ../requirements.txt
-# cd ..
-# sudo python setup.py install
--- scripts/remove.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Comment out the remove instructions for your operating system
-
-# Unix
-# ----
-# sudo rm /usr/local/bin/epipy
-# sudo pip uninstall epipy
