e03b5688c1f420890c1412229dbe4ea113e4563a
Christian Kaus <christian.kaus@hotmail.de>
2016-01-17 04:38:19 +0100

    add seir model

--- epipy/tests/test.py
+++ epipy/tests/test.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import testutils
 from epipy.utils import csvmanager
-from epipy.model import sir, sirs
+from epipy.model import sir, sirs, seir
 import numpy as np
 
 sir_simple = sir.Simple()
@@ -10,6 +9,7 @@ sir_vaccine = sir.Vaccine()
 sir_wbad = sir.WithBirthsAndDeaths()
 sirs_simple = sirs.Simple()
 sirs_wbad = sirs.WithBirthsAndDeaths()
+seir_simple = seir.Simple()
 # ======================================================================================================================
 # Data
 # ======================================================================================================================
@@ -49,6 +49,22 @@ ydata_2 = np.array([113, 60, 70, 140, 385, 2900, 4600, 5400, 5300, 6350, 5350, 4
 # testutils.plot('SIRS Base Model', xdata=time, ydata=[res4,res5],
 #                labels=['Simple', 'With Births And Deaths'])
 
+
+# -----------------------------------
+# Compare: http://www.public.asu.edu/~hnesse/classes/seir.html?
+# E0 = 1
+# beta = 0.9
+# gamma = 0.1
+# sigma = 0.1
+# mu = 0
+# nu = 0
+# time = np.arange(1, 120 + 1, 1)
+# N = 101
+# res6 = seir_simple.get_model(time, E0, N=101, beta=beta, gamma=gamma, sigma=sigma, mu=mu)
+# testutils.plot('SEIR Base Model', xdata=time, ydata=[res6], labels=['Simple'])
+
+
+
 # ======================================================================================================================
 # find optimize parameters
 # ======================================================================================================================
@@ -63,28 +79,38 @@ ydata_2 = np.array([113, 60, 70, 140, 385, 2900, 4600, 5400, 5300, 6350, 5350, 4
 # testutils.plot_with_data('With Optimize() - SIRS', xdata=xdata, ydata=ydata, results=[res4, res5],
 #                          labels=['Simple', 'With Births And Deaths'])
 
+# res6 = seir_simple.fit(xdata_2, ydata_2, N=10000)
+# testutils.plot_with_data('With Optimize() - SEIR', xdata=xdata_2, ydata=ydata_2, results=[res6],
+#                          labels=['Simple'])
 
 # ======================================================================================================================
 # use manual parameters for fitting
 # ======================================================================================================================
-N = 1
-beta = 0.50117819
-gamma = 0.10005569
-mu = 0.1
-f = 0.1
-nu = 0.1
-res1 = sir_simple.fit(xdata, ydata, N=N, beta=beta, gamma=gamma)
-res2 = sir_vaccine.fit(xdata, ydata, N=N, beta=beta, gamma=gamma, nu=nu)
-res3 = sir_wbad.fit(xdata, ydata, N=N, beta=beta, gamma=gamma, mu=mu)
-res4 = sirs_simple.fit(xdata, ydata, N=N, beta=beta, gamma=gamma, mu=mu)
-res5 = sirs_wbad.fit(xdata, ydata, N=N, beta=beta, gamma=gamma, mu=mu, f=f)
-
-testutils.plot_with_data('N=%s, beta=%s, gamma=%s, mu=%s, nu=%s, f=%s' % (N, beta, gamma, mu, nu, f), xdata, ydata,
-                         results=[res1, res2, res3, res4, res5],
-                         labels=['SIR Simple(b,g)', 'SIR Vac(b,g,n)', 'SIR Wbad(b,g,m)',
-                                 'SIRS Simple(b,g,m)', 'SIRS Wbad(b,g,m,f)'])
-
-
+# N = 1
+# beta = 0.50117819
+# gamma = 0.10005569
+# mu = 0.0007
+# f = 0
+# nu = 0
+# res1 = sir_simple.fit(xdata, ydata, N=N, beta=beta, gamma=gamma)
+# res2 = sir_vaccine.fit(xdata, ydata, N=N, beta=beta, gamma=gamma, nu=nu)
+# res3 = sir_wbad.fit(xdata, ydata, N=N, beta=beta, gamma=gamma, mu=mu)
+# res4 = sirs_simple.fit(xdata, ydata, N=N, beta=beta, gamma=gamma, mu=mu)
+# res5 = sirs_wbad.fit(xdata, ydata, N=N, beta=beta, gamma=gamma, mu=mu, f=f)
+#
+# testutils.plot_with_data('N=%s, beta=%s, gamma=%s, mu=%s, nu=%s, f=%s' % (N, beta, gamma, mu, nu, f), xdata, ydata,
+#                          results=[res1, res2, res3, res4, res5],
+#                          labels=['SIR Simple(b,g)', 'SIR Vac(b,g,n)', 'SIR Wbad(b,g,m)',
+#                                  'SIRS Simple(b,g,m)', 'SIRS Wbad(b,g,m,f)'])
+# #
+#
+# N = 10000
+# beta = 0.9
+# gamma = 0.1
+# mu = 0.0
+# sigma = 0.1
+# res6 = seir_simple.fit(xdata_2, ydata_2, N=N, beta=beta, gamma=gamma, sigma=sigma, mu=mu)
+# testutils.plot_with_data('SEIRS Simple', xdata_2, ydata_2, results=[res6], labels=['Simple'])
 
 
 
