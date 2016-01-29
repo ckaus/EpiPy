8b4833a839abd86a0909582743349b30c84dcaa7
Christian Kaus <christian.kaus@hotmail.de>
2016-01-29 21:53:23 +0100

    create help widget

---
+++ epipy/ui/view/helpwidget.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import os
+from PyQt4 import uic
+
+dir_name = os.path.dirname
+folder_path = os.path.join(dir_name(__file__), '')
+HelpWidgetUI, HelpWidgetBase = uic.loadUiType(os.path.join(folder_path, "helpwidget.ui"))
+
+
+class HelpWidget(HelpWidgetBase, HelpWidgetUI):
+    def __init__(self):
+        HelpWidgetBase.__init__(self)
+        self.setupUi(self)
