0c426823387d01dda15c7e44ffa09375aee0c3a6
Christian Kaus <christian.kaus@hotmail.de>
2016-02-15 12:34:24 +0100

    change help widget - content

--- epipy/ui/view/helpwidget.py
+++ epipy/ui/view/helpwidget.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import os
-from PyQt4 import uic
+from PyQt4 import QtGui, QtCore, QtWebKit
 
 dir_name = os.path.dirname
-folder_path = os.path.join(dir_name(__file__), '')
-HelpWidgetUI, HelpWidgetBase = uic.loadUiType(os.path.join(folder_path, "helpwidget.ui"))
+folder_path = os.path.join(dir_name(dir_name(dir_name(__file__))), '')
 
 
-class HelpWidget(HelpWidgetBase, HelpWidgetUI):
+class HelpWidget(QtGui.QWidget):
     """
     This class represents the Help widget.
 
@@ -16,5 +15,9 @@ class HelpWidget(HelpWidgetBase, HelpWidgetUI):
     """
 
     def __init__(self):
-        HelpWidgetBase.__init__(self)
-        self.setupUi(self)
+        QtGui.QWidget.__init__(self)
+        layout = QtGui.QVBoxLayout()
+        web_view = QtWebKit.QWebView()
+        web_view.load(QtCore.QUrl(os.path.join(folder_path + 'resources', "manual.htm")))
+        layout.addWidget(web_view)
+        self.setLayout(layout)
