da4a288628b975de7195bffb74facc855e488009
Christian Kaus <christian.kaus@hotmail.de>
2016-09-28 22:39:29 +0200

    update

--- epipy/__init__.py
+++ epipy/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = '1.0.0'
 __author__ = 'ckaus, mitalbert, yenarhee'
-__author_email__ = 'christian.kaus@hotmail.de'
+__author_email__ = 'kiasu_wc3@hotmail.de'
 __description__ = 'Python GUI tool for fitting epidemic models.'
-__maintainer__ = __author__
+__maintainer__ = 'ckaus'
 __maintainer_email__ = __author_email__
